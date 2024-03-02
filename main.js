68e8252db245ffb6bd34d2b6dc18bd6d2dad0c6f
Ibtehaz Ali Chowdhury <ibtehaz.chowdhury@gmail.com>
2024-03-02 21:58:13 +1100

    #1 Sprite class implemented #2 Game Loop features implemented

---
+++ main.js
@@ -0,0 +1,51 @@
+import { GameLoop } from "./src/GameLoop";
+import { Sprite } from "./src/Sprite";
+import { Vector2 } from "./src/Vector2";
+import { resources } from "./src/resource";
+import "./style.css";
+
+const canvas = document.querySelector("#game-canvas");
+
+const ctx = canvas.getContext("2d");
+
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180),
+});
+
+const groundSprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180),
+});
+
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+});
+
+const shadowSprite = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32),
+});
+
+const heroPos = new Vector2(16 * 6, 16 * 5);
+
+const update = () => {};
+
+const draw = () => {
+  skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
+
+  const heroOffset = new Vector2(-8, -21);
+  const heroPosX = heroPos.x + heroOffset.x;
+  const heroPosY = heroPos.y + heroOffset.y;
+
+  shadowSprite.drawImage(ctx, heroPosX, heroPosY);
+  hero.drawImage(ctx, heroPosX, heroPosY);
+};
+
+const gameLoop = new GameLoop(update, draw);
+gameLoop.start();
